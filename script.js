78610be046c31f56adb4bf52a7cbbf2fff60c339
coderkhan1 <alham8568@gmail.com>
2024-10-11 08:25:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("YESbtn").addEventListener("click"
+    , function(){
+
+document.getElementById("result").innerHTML ="YAY! 😍 you are the love of my life!"
+    });
+
+document.getElementById("NObtn").addEventListener("click",
+    function(){
+        document.getElementById("result").innerHTML = "WHAT? 💔 But I'LL still love you and hit the gym.";
+    })
